26357c4a1bfe27686812d57bee50739e1f80052e
seftiansyahm <seftirobi.m@gmail.com>
2023-06-28 15:54:59 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,130 @@
+<div align="center">
+ 
+```
+  █████████  ███████████   ██████   ██████
+ ███░░░░░███░░███░░░░░███ ░░██████ ██████ 
+░███    ░░░  ░███    ░███  ░███░█████░███ 
+░░█████████  ░██████████   ░███░░███ ░███ 
+ ░░░░░░░░███ ░███░░░░░███  ░███ ░░░  ░███ 
+ ███    ░███ ░███    ░███  ░███      ░███ 
+░░█████████  █████   █████ █████     █████
+ ░░░░░░░░░  ░░░░░   ░░░░░ ░░░░░     ░░░░░ 
+```
+</div> 
+
+# SimpleVideoDownloader
+
+SimpleVideoDownloader is a simple script for downloading videos in 360p resolution (default) based on the list of YouTube links in the "links.txt" file.And it can also directly convert to MP3 files.
+
+- Python 3.
+- Easy to use.
+- Downloading videos from the links provided in the `links.txt` file in 360p resolution (You can modify it within the script).
+- It can directly convert to MP3 files.
+- This script is assisted by Chat GPT.
+
+## NOTE
+```
+- Not all websites use the same format or provide the same options for downloading videos. For example, this code may not work properly for downloading videos from websites that employ different download methods or are incompatible with youtube_dl
+- Tested on YouTube 
+```
+
+## HOW TO USE
+
+- First, install Python (if you haven't already).
+- Simply edit the `links.txt` file, enter each YouTube link on a separate line.
+- Run the script and enter the prompted command.
+
+```sh
+$ python main.py
+```
+- choose an option
+  
+## SCREENSHOOTS
+![image](https://github.com/Seftirobim/SimpleVideoDownloader/assets/16395774/e3508034-111f-46d6-9046-9342bb12659c)
+![image](https://github.com/Seftirobim/SimpleVideoDownloader/assets/16395774/b9fdc59a-049b-4487-9a58-74429617fcf9)
+
+## IMPORTANT
+is highly recommended that you install FFmpeg and FFprobe. FFprobe comes when you install the  FFmpeg package.
+
+**To install FFmpeg on a MacOS machine, use the following command:**
+```
+brew install ffmpeg
+```
+**To install FFmpeg on a Linux machine (Ubuntu 22.04), use the following command:** 
+```sh
+$ sudo apt install ffmpeg
+```
+**To install FFmpeg on a Windows machine, use the following command:**
+<details>
+<summary>First step</summary>
+
+ go to [this repository](https://github.com/BtbN/FFmpeg-Builds/releases)
+</details>
+
+<details>
+<summary>Second step</summary>
+
+ Choose your release, download the 7z or zip file.
+
+![1](https://github.com/Seftirobim/SimpleVideoDownloader/assets/16395774/b5f272fa-7ebe-4208-ab59-87e606e54b27)
+</details>
+
+<details>
+<summary>Third step</summary>
+ 
+ * Download the package and save it anywhere you want and uncrompress it. For example, we open the compression in `C:/ffmpeg`
+ * Record the path `C:\ffmepg\bin)` and head over to “Edit the system environment variables".
+ 
+![copy path](https://github.com/Seftirobim/SimpleVideoDownloader/assets/16395774/5e569130-9776-4ecd-88dc-3fa37c2bb0d2)
+</details>
+
+<details>
+<summary>Fourth step</summary>
+ 
+ To open this, go to the search bar on Windows and type “path” or "envi"
+ 
+![3](https://github.com/Seftirobim/SimpleVideoDownloader/assets/16395774/e87b5c57-e4d1-4d7f-b3e6-b54868c99549)
+</details>
+<details>
+<summary>Fifth step</summary>
+ 
+ In System Properties > Advanced, head over to “Environment Variables”
+ 
+![4](https://github.com/Seftirobim/SimpleVideoDownloader/assets/16395774/9f19ddd8-4300-425c-a35c-135096e1f7b9)
+</details>
+<details>
+<summary>Sixth step</summary>
+ 
+ In Environment Variables, under “User variables for Administrators” choose Path (1) > then click on “Edit”.
+ 
+![5](https://github.com/Seftirobim/SimpleVideoDownloader/assets/16395774/c44bceb0-11d3-43b9-8fc4-24a8c35a404e)
+</details>
+<details>
+<summary>Seventh step</summary>
+ 
+ The new “Edit Enviromnet variable” window will open. Click on New (1) > Enter the Path where FFmpeg is stored (2) > Click on Ok (3).
+ 
+![6](https://github.com/Seftirobim/SimpleVideoDownloader/assets/16395774/b0e94f1c-a5f6-4201-8df7-4898909571b5)
+</details> 
+<details>
+<summary>Eighth step</summary>
+ 
+- Now, test the FFmpeg configuration from the Windows command prompt. Open the “cmd” and type ‘ffmpeg’. You should get an output such as the one below.
+ 
+![7](https://github.com/Seftirobim/SimpleVideoDownloader/assets/16395774/4dd3289c-a4ae-430d-b73e-97ffa0d43d4f)
+
+- **If an error occurs, try restarting the machine.** 
+</details>   
+
+
+## INSTALLATION REQUIREMENTS 
+
+Windows:
+```sh
+$ pip install -r requirements.txt
+```
+
+Linux:
+```sh
+$ pip3 install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,179 @@
+import yt_dlp
+import os
+from rich.console import Console
+from rich.markdown import Markdown
+import webbrowser
+from rich.table import Table,box
+from rich.align import Align
+
+console = Console()
+menu_options = {
+   1: 'Open links.txt',
+   2: 'Download Video(s)',
+   3: 'Download Video(s) and Convert to Mp3',
+   4: 'Exit'
+}
+
+title_markdown = """
+# Simple Video Downloader & Mp3 Converter
+"""
+md = Markdown(title_markdown)
+
+SRM_logo = """
+  █████████  ███████████   ██████   ██████
+ ███░░░░░███░░███░░░░░███ ░░██████ ██████ 
+░███    ░░░  ░███    ░███  ░███░█████░███ 
+░░█████████  ░██████████   ░███░░███ ░███ 
+ ░░░░░░░░███ ░███░░░░░███  ░███ ░░░  ░███ 
+ ███    ░███ ░███    ░███  ░███      ░███ 
+░░█████████  █████   █████ █████     █████
+ ░░░░░░░░░  ░░░░░   ░░░░░ ░░░░░     ░░░░░                                                                                            
+"""
+class MyLogger:
+    def debug(self, msg):
+        # For compatibility with youtube-dl, both debug and info are passed into debug
+        # You can distinguish them by the prefix '[debug] '
+        if msg.startswith('[debug] '):
+            pass
+        else:
+            self.info(msg)
+
+    def info(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+class MyCustomPP(yt_dlp.postprocessor.PostProcessor):
+    def run(self, info):
+        self.to_screen('Doing stuff')
+        return [], info
+
+def print_title():
+    console.print(SRM_logo, justify="center",style="bright_cyan")
+    console.print('https://github.com/Seftirobim/SimpleYTDownloader', justify="center", style="bold grey100")
+    console.print(md)
+
+def print_menu():
+    table = Table(box=box.SIMPLE_HEAD)
+    table.add_column("No",justify="center",style="bold bright_yellow")
+    table.add_column("Options",justify="center",style="gold1")
+
+    for k in menu_options.keys():
+        table.add_row('['+str(k)+']',Align(menu_options[k],'left'))
+    console.print(table)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print(' Success')
+
+def download_videos(links, save_folder,convert=False):
+    failed_links = []
+    
+    for link in links :
+        try:
+            if convert == False:
+                ydl_opts = {
+                    'format': 'bestvideo[height<=360][ext=mp4]+bestaudio[ext=m4a]/best[height<=360][ext=mp4]',
+                    'outtmpl': os.path.join(save_folder, '%(title)s.%(ext)s'),
+                    'progress_hooks': [my_hook],
+                    'quiet': True,
+                    'ignoreerrors': True,
+                }
+            else:
+                ydl_opts = {
+                    'format': 'bestaudio/best',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '192',
+                    }],
+                    'outtmpl': os.path.join(save_folder, '%(title)s.%(ext)s'),
+                    'progress_hooks': [my_hook],
+                    'quiet': True,
+                    'ignoreerrors': True
+                }  
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.add_post_processor(MyCustomPP(), when='pre_process')
+                ydl.download([link])
+
+        except Exception as e:
+            error_message = e
+            failed_links.append(link)
+            console.print(f"Error downloading video from {link}: {str(e)}",style="red1")
+        
+            
+
+    return failed_links
+
+
+def run(C=False):
+    current_path = os.getcwd()
+    #get links.txt file from path
+    file_path = ''.join([current_path,'/links.txt'])
+
+    with open(file_path, "r") as file:
+        video_links = file.read().splitlines()
+
+    console.print(f"\nTotal link(s): {len(video_links)} link",style="plum2")
+
+    #get save path
+    if C == False:
+        save_path = ''.join([current_path,"/downloads/video"])
+        failed_videos = download_videos(video_links, save_path,False)
+    else:
+        save_path = ''.join([current_path,"/downloads/mp3"])
+        failed_videos = download_videos(video_links, save_path,True)
+
+    # Display the failed videos, if any
+    if failed_videos:
+        print("\nList of videos that failed to download :")
+        for link in failed_videos:
+            print(link)
+    else:
+        if C == False :
+            console.print("\nAll video(s) have been successfully downloaded.!",style="bold spring_green1")
+        else:    
+            console.print("\nAll video(s) have been successfully downloaded and converted!",style="bold spring_green1")
+        
+        while True:
+            try:
+                open_folder = input("Would you like to open the folder? (y/n): ")
+            except:
+                console.print(f"Invalid Input ! Enter (y/n)",style="red1")
+            if open_folder.lower() == "y":
+                webbrowser.open(os.path.abspath(save_path))
+                break
+            elif open_folder.lower() == "n":
+                console.print(f"Exit !",style="yellow")
+                break
+            else:
+                console.print(f"Invalid Input ! Enter (y/n)",style="red1")
+
+if __name__ == "__main__":
+    while True:
+
+        try:
+            print_title()
+            print_menu()
+            prompt = int(input("Select an option between [1-4] : "))
+        except:
+            print("Invalid Input !")
+        if prompt == 1 :
+            file= 'links.txt'
+
+            open_file =  os.startfile(file) 
+        elif prompt == 2:
+            run(False)            
+            break
+        elif prompt == 3:
+            run(True)            
+            break
+        elif prompt == 4:
+            console.print("Exit !",style="red1")
+            break
+        else:
+            print('Invalid Input, Enter (y/n)')        
